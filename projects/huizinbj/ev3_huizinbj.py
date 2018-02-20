06414df57d56379e2f8d8101d3ba6e9078d7d138
huizinbj <huizinbj@users.noreply.github.com>
2018-02-19 21:06:16 -0500

    Finished

--- projects/huizinbj/ev3_huizinbj.py
+++ projects/huizinbj/ev3_huizinbj.py
@@ -1,41 +1,38 @@
+"""
+Code to be ran on the ev3 for Canvas control. The robot is receiving waypoint
+selected on the computers canvas and converts them in a 4ft by 4 ft area.
+
+Author: Brett Huizinga
+"""
 import ev3dev.ev3 as ev3
 import robot_controller as robo
 import mqtt_remote_method_calls as com
 import time
 
-class DataContainer(object):
-    """ Helper class that might be useful to communicate between different callbacks."""
-    def __init__(self):
-        self.running = True
 
 def main():
     print("--------------------------------------------")
-    print(" Running Tracker Control")
+    print(" Running Canvas Control")
     print(" Press the touch sensor to exit")
     print("--------------------------------------------")
-    ev3.Sound.speak("Tracker Control").wait()
+    ev3.Sound.speak("Canvas Control").wait()
     print("Press the touch sensor to exit this program.")
 
     # Creates Mqtt Client and Snatcher Bot
     robot = robo.Snatch3r()
-    dc = DataContainer()
-
     mqtt_client2 = com.MqttClient(robot)
     robot.set_mqtt_client(mqtt_client2)
     mqtt_client2.connect_to_pc()
 
-    btn = ev3.Button()
-    btn.on_up = handle_up_button()
-    while dc.running:
-        btn.process()  # This command is VERY important when using button callbacks!
+    while robot.running:
         time.sleep(0.01)
+        if robot.ir_sensor.proximity < 10:
+            robot.stop()
+        if robot.touch_sensor.is_pressed:
+            print("I have the object")
+            time.sleep(0.01)
+            robot.arm_down()
 
 
-def handle_up_button(button_state):
-    if button_state:
-        print("Up button is pressed")
-        mqtt_client2.connect_to_pc
 
-    else:
-        print("Up button was released")
 main()
--- projects/huizinbj/pc_huizinbj.py
+++ projects/huizinbj/pc_huizinbj.py
@@ -1,3 +1,10 @@
+"""
+Code to be ran on a computer for Canvas control. This creates an mqtt_remote
+ for the robot along with creating the canvas that sends mqtt messages to the
+ robot for it to drive to by the user clicking on its interface.
+
+Author: Brett Huizinga
+"""
 import tkinter
 from tkinter import ttk
 import mqtt_remote_method_calls as com
@@ -45,35 +52,35 @@ def main():
 
     # Creates Tab One
     tab1 = ttk.Frame(tab_control)  # Create a tab
-    tab_control.add(tab1, text='Remote')  # Add the tab
+    tab_control.add(tab1, text='Arm Control')  # Add the tab
     tab_control.pack(expand=1, fill="both")  # Pack to make visible
 
     # Creates Tab Two
     tab2 = ttk.Frame(tab_control)
-    tab_control.add(tab2, text='Arm Control')
+    tab_control.add(tab2, text='Program')
     tab_control.pack(expand=2, fill="both")
 
     # Creates Tab Three
     tab3 = ttk.Frame(tab_control)
-    tab_control.add(tab3, text='Program')
+    tab_control.add(tab3, text='Remote')
     tab_control.pack(expand=2, fill="both")
 
     # Left speed box
-    left_speed_label = ttk.Label(tab1, text="Left")
+    left_speed_label = ttk.Label(tab3, text="Left")
     left_speed_label.grid(row=0, column=0)
-    left_speed_entry = ttk.Entry(tab1, width=8)
+    left_speed_entry = ttk.Entry(tab3, width=8)
     left_speed_entry.insert(0, "600")
     left_speed_entry.grid(row=1, column=0)
 
     # Right speed box
-    right_speed_label = ttk.Label(tab1, text="Right")
+    right_speed_label = ttk.Label(tab3, text="Right")
     right_speed_label.grid(row=0, column=2)
-    right_speed_entry = ttk.Entry(tab1, width=8, justify=tkinter.RIGHT)
+    right_speed_entry = ttk.Entry(tab3, width=8, justify=tkinter.RIGHT)
     right_speed_entry.insert(0, "600")
     right_speed_entry.grid(row=1, column=2)
 
     # Forward Button on Gui
-    forward_button = ttk.Button(tab1, text="Forward")
+    forward_button = ttk.Button(tab3, text="Forward")
     forward_button.grid(row=2, column=1)
     forward_button['command'] = lambda: send_forward(mqtt_client2,
                                                      int(left_speed_entry.get()
@@ -87,7 +94,7 @@ def main():
                                                  int(right_speed_entry.get())))
 
     # Left Button on Gui
-    left_button = ttk.Button(tab1, text="Left")
+    left_button = ttk.Button(tab3, text="Left")
     left_button.grid(row=3, column=0)
     left_button['command'] = lambda: send_left(mqtt_client2,
                                                int(left_speed_entry.get()),
@@ -97,13 +104,13 @@ def main():
                                                 int(right_speed_entry.get())))
 
     # Stop Button On Gui
-    stop_button = ttk.Button(tab1, text="Stop")
+    stop_button = ttk.Button(tab3, text="Stop")
     stop_button.grid(row=3, column=1)
     stop_button['command'] = lambda: stopbot(mqtt_client2)
     root.bind('<space>', lambda event: stopbot(mqtt_client2))
 
     # Right Button On Gui
-    right_button = ttk.Button(tab1, text="Right")
+    right_button = ttk.Button(tab3, text="Right")
     right_button.grid(row=3, column=2)
     right_button['command'] = lambda: send_right(mqtt_client2,
                                                  int(left_speed_entry.get()),
@@ -114,7 +121,7 @@ def main():
                                                       )))
 
     # Back Button on Gui
-    back_button = ttk.Button(tab1, text="Back")
+    back_button = ttk.Button(tab3, text="Back")
     back_button.grid(row=4, column=1)
     back_button['command'] = lambda: send_back(mqtt_client2,
                                                int(left_speed_entry.get()),
@@ -124,19 +131,19 @@ def main():
                                                 int(right_speed_entry.get())))
 
     # Up Button On Gui
-    up_button = ttk.Button(tab2, text="Arm Up")
+    up_button = ttk.Button(tab1, text="Arm Up")
     up_button.grid(row=0, column=0)
     up_button['command'] = lambda: send_up(mqtt_client2)
     root.bind('<u>', lambda event: send_up(mqtt_client2))
 
     # Down Button On Gui
-    down_button = ttk.Button(tab2, text="Arm Down")
+    down_button = ttk.Button(tab1, text="Arm Down")
     down_button.grid(row=1, column=0)
     down_button['command'] = lambda: send_down(mqtt_client2)
     root.bind('<j>', lambda event: send_down(mqtt_client2))
 
     # Quit Button on Gui
-    quit_button = ttk.Button(tab3, text="Quit")
+    quit_button = ttk.Button(tab2, text="Quit")
     quit_button.grid(row=0, column=0)
     quit_button['command'] = (lambda: quit_program(mqtt_client2))
 
